b847491622afc3242ef4361b045491ea95445ad9
Evan Alves <evanalves@Evans-MacBook-Air.local>
2021-09-24 15:42:22 -0400

    styled media page

--- client/src/Data/Photos.js
+++ client/src/Data/Photos.js
@@ -1,46 +1,43 @@
 
 export const Photos = [
   {
-    link: '',
+    link: 'https://www.facebook.com/media/set/?set=a.228690670626695.1073741833.173725192789910&type=3',
     year: '2013',
     className: 'photo-text'
   },
   {
-    link: '',
+    link: 'https://www.facebook.com/media/set/?set=a.389148097914284.1073741834.173725192789910&type=3',
     year: '2014',
     className: 'photo-text'
   },
   {
-    link: '',
+    link: 'https://www.facebook.com/media/set/?set=a.529102450585514.1073741838.173725192789910&type=3',
     year: '2015',
     className: 'photo-text'
   },
   {
-    link: '',
+    link: 'https://www.facebook.com/media/set/?set=a.389148097914284.1073741834.173725192789910&type=3',
     year: '2016',
     className: 'photo-text'
   },
-]
-
-export const PhotosRow2 = [
   {
-    link: '',
+    link: 'https://www.facebook.com/media/set/?vanity=WickedInPink&set=a.1434329276729489',
     year: '2017',
     className: 'photo-text'
   },
   {
-    link: '',
+    link: 'https://www.facebook.com/media/set/?vanity=WickedInPink&set=a.1434352776727139',
     year: '2018',
     className: 'photo-text'
   },
   {
-    link: '',
+    link: 'https://www.facebook.com/media/set/?vanity=WickedInPink&set=a.1479515782210838',
     year: '2019',
     className: 'photo-text'
-  },
-  {
-    link: '',
-    year: '2020',
-    className: 'photo-text'
-  },
+  }
+]
+
+export const PhotosRow2 = [
+  
+  
 ]
--- client/src/Screens/Media/Media.jsx
+++ client/src/Screens/Media/Media.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import "./Media.css"
 import { Photos } from '../../Data/Photos'
-import { PhotosRow2 } from '../../Data/Photos'
 
 export default function Media() {
   return (
@@ -20,19 +19,28 @@ export default function Media() {
         )
       })}
       </div>
-      <div className="year-container">
-      {PhotosRow2.map((item, index) => {
-        return (
-          <div key={index} className="yearsrow2">
-            <a href={item.link} target="_blank">
-              <h1 className={item.className}>
-                {item.year}
-              </h1>
-            </a>
-          </div>
-        )
-        })}
-        </div>
+
+      <div className="video-container">
+        <iframe className="haymondvideo"
+          width="560" height="315"
+          src="https://www.youtube.com/embed/l2q-UL5RNrE"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
+        </iframe>
+        <iframe className="wip2019"
+          width="560" height="315"
+          src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FWickedInPink%2Fvideos%2F715366702659513%2F&show_text=false&width=560&t=0"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
+        </iframe>
+      </div>
+
+      <div>
+        <img className="photo-ad" src="https://static.wixstatic.com/media/3b1f02_47c676207adb4bb1b052b061e32120cb.jpg/v1/fill/w_834,h_109,al_c,q_80,usm_0.66_1.00_0.01/3b1f02_47c676207adb4bb1b052b061e32120cb.webp" />
+      </div>
     </div>
   )
 }
