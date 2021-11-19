871dc853733a866fd354fe3c8a664c0ddc98be51
Evan Alves <evanalves@Evans-MacBook-Air.local>
2021-11-19 15:42:06 -0500

    bug with input fields

--- client/src/Shop/Shop.jsx
+++ client/src/Shop/Shop.jsx
@@ -8,44 +8,49 @@ import Checkout from './ShopComponents/CheckoutForm/Checkout/Checkout'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-export default function Shop() {
+const App = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const [products, setProducts] = useState([])
-  const [cart, setCart] = useState({})
-  const [order, setOrder] = useState({})
-  const [errorMessage, setErrorMessage] = useState('')
-  
   const fetchProducts = async () => {
-    const { data } = await commerce.products.list()
-    setProducts(data)
-  }
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
 
   const fetchCart = async () => {
-    setCart(await commerce.cart.retrieve())
-  }
+    setCart(await commerce.cart.retrieve());
+  };
 
   const handleAddToCart = async (productId, quantity) => {
-    const { cart } = await commerce.cart.add(productId, quantity)
-    setCart(cart)
-  }
+    const item = await commerce.cart.add(productId, quantity);
 
-  const handleUpdateCartQty = async (productId, quantity) => {
-    const { cart } = await commerce.cart.update(productId, { quantity })
-    setCart(cart)
-  }
-  
-  const handleRemoveFromCart = async (productId) => {
-    const { cart } = await commerce.cart.remove(productId)
-    setCart(cart)
-  }
+    setCart(item.cart);
+  };
 
-  const handleEmptyCart = async() => {
-    const { cart } = await commerce.cart.empty()
-    setCart(cart)
-  }
+  const handleUpdateCartQty = async (lineItemId, quantity) => {
+    const response = await commerce.cart.update(lineItemId, { quantity });
+
+    setCart(response.cart);
+  };
+
+  const handleRemoveFromCart = async (lineItemId) => {
+    const response = await commerce.cart.remove(lineItemId);
+
+    setCart(response.cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
+
+    setCart(response.cart);
+  };
 
   const refreshCart = async () => {
-    const newCart = await commerce.cart.refresh().then((cart) => console.log(cart));
+    const newCart = await commerce.cart.refresh();
 
     setCart(newCart);
   };
@@ -62,43 +67,33 @@ export default function Shop() {
     }
   };
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchProducts();
-    fetchCart()
-  }, [])
+    fetchCart();
+  }, []);
 
-  // console.log(cart)
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
 
   return (
     <Router>
     <div className="shop">
       <div className="shop-wrapper">
-        <NavbarShop totalItems={cart.total_items} />
-          <Switch>
-            <Route exact path="/shop">
-              <Products products={products} onAddToCart={handleAddToCart} />
-            </Route>
-            
-            <Route exact path="/cart">
-              <Cart
-                cart={cart}
-                handleUpdateCartQty={handleUpdateCartQty}
-                handleRemoveFromCart={handleRemoveFromCart}
-                handleEmptyCart={handleEmptyCart}
-              />
-            </Route>
-
-            <Route exact path="/checkout">
-              <Checkout
-                cart={cart}
-                order={order}
-                handleCaptureCheckout={handleCaptureCheckout}
-                error={errorMessage}
-              />
-            </Route>
-          </Switch>
+        <NavbarShop totalItems={cart.total_items} handleDrawerToggle={handleDrawerToggle} />
+        <Switch>
+          <Route exact path="/shop">
+            <Products products={products} onAddToCart={handleAddToCart} handleUpdateCartQty />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />
+          </Route>
+          <Route path="/checkout" exact>
+            <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />
+          </Route>
+        </Switch>
       </div>
     </div>
     </Router>
-  )
-}
+  );
+};
+
+export default App;
--- client/src/Shop/ShopComponents/CheckoutForm/PaymentForm.jsx
+++ client/src/Shop/ShopComponents/CheckoutForm/PaymentForm.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { Typography, Button, Divider } from '@material-ui/core'
-import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
-import Review from './Review'
+import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
+import Review from './Review';
 
-const PaymentForm = ({ checkoutToken, shippingData, backStep, handleCaptureCheckout, nextStep }) => {
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
-  const handleSubmit =  async (event, elements, stripe) => {
+const PaymentForm = ({ checkoutToken, nextStep, backStep, shippingData, onCaptureCheckout }) => {
+  console.log(shippingData)
+  const handleSubmit = async (event, elements, stripe) => {
     event.preventDefault();
 
     if (!stripe || !elements) return;
@@ -16,9 +17,9 @@ const PaymentForm = ({ checkoutToken, shippingData, backStep, handleCaptureCheck
     const cardElement = elements.getElement(CardElement);
 
     const { error, paymentMethod } = await stripe.createPaymentMethod({ type: 'card', card: cardElement });
-    
+
     if (error) {
-      console.log(error)
+      console.log('[error]', error);
     } else {
       const orderData = {
         line_items: checkoutToken.live.line_items,
@@ -33,46 +34,48 @@ const PaymentForm = ({ checkoutToken, shippingData, backStep, handleCaptureCheck
           town_city: shippingData.city,
           county_state: shippingData.shippingSubdivision,
           postal_zip_code: shippingData.zip,
-          country: shippingData.shippingCountry,
+          country: shippingData.shippingCountry
         },
-        fulfillment: { shipping_method: shippingData.shippingOption },
+        fulfillment: {
+          shipping_method: shippingData.shippingOption
+        },
         payment: {
           gateway: 'stripe',
           stripe: {
-            payment_method_id: paymentMethod.id
-          }
-        }
-      }
-      handleCaptureCheckout(checkoutToken.id, orderData)
-      nextStep()
+            payment_method_id: paymentMethod.id,
+          },
+        },
+      };
+
+      onCaptureCheckout(checkoutToken.id, orderData);
+      nextStep();
     }
-  }
+  };
 
   return (
     <>
       <Review checkoutToken={checkoutToken} />
       <Divider />
-      <Typography variant="h6" gutterBottom style={{margin: '20px 0'}}>
-        Payment Method
-      </Typography>
+      <Typography variant="h6" gutterBottom style={{ margin: '20px 0' }}>Payment method</Typography>
       <Elements stripe={stripePromise}>
-        <ElementsConsumer>
-          {({ elements, stripe }) => (
-            <form onSubmit={(e) => handleSubmit(e, elements, stripe)}>
-              <CardElement />
-              <br /> <br />
-              <div style={{ display: 'flex', justifyContent: 'space-between'}}>
-                <Button variant="outlined" onClick={backStep}>Back</Button>
-                <Button type="submit" variant="contained" disabled={!stripe} color="primary">
-                  Pay { checkoutToken.live.subtotal.formatted_with_symbol }
-                </Button>
-              </div>
-            </form>
-          )}
+        <ElementsConsumer>{({ elements, stripe }) => (
+          <form onSubmit={(e) => handleSubmit(e, elements, stripe)}>
+            <CardElement />
+            <br /> <br />
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button variant="outlined" onClick={backStep}>Back</Button>
+              <Button type="submit" variant="contained" disabled={!stripe} color="primary">
+                Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+              </Button>
+            </div>
+          </form>
+        )}
         </ElementsConsumer>
       </Elements>
     </>
-  )
-}
+  );
+};
 
-export default PaymentForm
+export default PaymentForm;
+
+
